0eb6c68ad4df3fcb3e724fbf08a90ae11da7cdb2
Ayomide Daniel <adebalanced02@gmail.com>
2018-07-01 17:06:23 +0100

    used index db to store currency rates

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
+window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+
 /*  Base Url of api */
 const baseUrl = 'https://free.currencyconverterapi.com/api/v5'
+
+let db;
+request = window.indexedDB.open("converter", 1);
+request.onsuccess = event => {
+  db = event.target.result
+}
+request.onupgradeneeded = event => {
+  db = event.target.result
+  let objectStore = db.createObjectStore("convertTable", {
+    keyPath: "name"
+  })
+  objectStore.createIndex('name', 'name', {
+    unique: false
+  })
+  objectStore.transaction.oncomplete = event => {
+    let currencyTableStore = db.transaction('convertTable', 'readwrite').objectStore('convertTable')
+  }
+}
 
 let state = {
   convertTo: '',
   convertFrom: '',
   amount: ''
 }
-
 /*  Utility Function for Calling Api */
 const callApi = (url, data, method) => {
   console.log("Calling API... " + url);
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     let options = {
       method: method || "GET",
     };
@@ -32,12 +51,13 @@ const callApi = (url, data, method) => {
 /*  Utility Function to handle select change */
 const handleInputChange = (key, value) => {
   state[key] = value
-  console.log(state)
 }
 
 /*  Fill UI Select buttons with currency values */
 const fillSelectButtons = () => {
-  getCurrencies().then(({results}) => {
+  getCurrencies().then(({
+    results
+  }) => {
     let html = ''
     for (let key in results) {
       let obj = results[key]
@@ -49,11 +69,11 @@ const fillSelectButtons = () => {
         sendOutputFeedback('')
         let className = (currencySelect.className.split(' ')[0])
         let value = currencySelect.value
-        handleInputChange( className, value )
+        handleInputChange(className, value)
       })
       currencySelect.innerHTML += html
     })
-    }).catch(err => console.log(err))
+  }).catch(err => console.log(err))
 }
 
 /*  Get full list of currencies from api */
@@ -62,31 +82,57 @@ const getCurrencies = () => {
     let options = {
       method: "GET",
     }
-    callApi('/currencies').then(data => resolve(data)).catch(err => reject(err))
+    callApi('/currencies').then(data => {
+      resolve(data)
+    }).catch(err => reject(err))
   })
 }
 
 /*  Convert Currency */
 const convertCurrency = () => {
-  let { amount, convertFrom, convertTo } = state
+  let {
+    amount,
+    convertFrom,
+    convertTo
+  } = state
   check(state).then(checkResult => {
     if (checkResult === true) {
       convertFrom = encodeURIComponent(convertFrom)
       convertTo = encodeURIComponent(convertTo)
       let query = `${convertFrom}_${convertTo}`
-      callApi(`/convert?q=${query}&compact=ultra`).then(data => {
-        console.log(data)
-        let conversionCurrency = Object.keys(data)
-        let conversionRate = Object.values(data)
-        let conversion = conversionRate[0] * Number(amount)
-        sendOutputFeedback(`${conversion}`)
-      }).then(err => console.log(err))
+      let req = db.transaction("convertTable").objectStore("convertTable").get(`${convertFrom}_${convertTo}`)
+      req.onsuccess = event => {
+        let result = req.result
+        if (result) {
+          showConversion(result, amount)
+        } else
+          callApi(`/convert?q=${query}&compact=ultra`).then(data => {
+            data = {
+              name: Object.keys(data)[0],
+              rate: Object.values(data)[0]
+            }
+            let transaction = db.transaction(['convertTable'], "readwrite")
+            transaction.oncomplete = event => {
+              let currencyTableStore = db.transaction('convertTable', 'readwrite').objectStore('convertTable')
+              currencyTableStore.add(
+                data
+              )
+            }
+            showConversion(data, amount)
+          }).then(err => console.log(err))
+      }
     }
   })
 }
 
+showConversion = (data, amount) => {
+  console.log(data.rate)
+  let conversion = data.rate * Number(amount)
+  sendOutputFeedback(`${conversion}`)
+}
+
 /*  Utility function to check values, takes an object */
-function check (obj = {}) {
+function check(obj = {}) {
   let checkArr = Object.values(obj)
   return new Promise((resolve, reject) => {
     for (let key in obj) {
@@ -117,11 +163,11 @@ addListenerToAmountInputField = () => {
 /*  Utility Function to send output back to client */
 sendOutputFeedback = (value) => {
   let output = document.getElementsByClassName('output')[0]
-  output.innerHTML=value
+  output.innerHTML = value
 }
 
 window.onload = () => {
-  fillSelectButtons()  
+  fillSelectButtons()
   addListenerToConvertButton()
   addListenerToAmountInputField()
 }
--- sw.js
+++ sw.js
@@ -1,13 +1,17 @@
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('airhorner').then(cache => {
+    caches.open('balancedmws').then(cache => {
+      
       return cache.addAll([
         '/',
         '/index.html',
         '/styles.css',
         '/script.js',
+        'https://free.currencyconverterapi.com/api/v5/currencies',
+        'https://fonts.googleapis.com/css?family=Roboto:400,300,500,700,100',
+        'https://fonts.googleapis.com/css?family=Lobster+Two|Merriweather|Neuton|Redressed',
       ]);
-    })
+    }).catch(err => console.log(err))
   );
 });
 
